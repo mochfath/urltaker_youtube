c5ca4d4de13b3296971ce2fb40c090548e510432
mochfath <efath3214@gmail.com>
2015-08-04 21:17:30 +0700

    1.9.5

---
+++ my_uploads.js
@@ -0,0 +1,87 @@
+// Define some variables used to remember state.
+var playlistId, nextPageToken, prevPageToken;
+
+// After the API loads, call a function to get the uploads playlist ID.
+function handleAPILoaded() {
+  requestUserUploadsPlaylistId();
+}
+
+// Call the Data API to retrieve the playlist ID that uniquely identifies the
+// list of videos uploaded to the currently authenticated user's channel.
+function requestUserUploadsPlaylistId() {
+  // See https://developers.google.com/youtube/v3/docs/channels/list
+  var request = gapi.client.youtube.channels.list({
+    mine: true,
+    part: 'contentDetails'
+  });
+  request.execute(function(response) {
+    playlistId = response.result.items[0].contentDetails.relatedPlaylists.uploads;
+    requestVideoPlaylist(playlistId);
+  });
+}
+
+// Retrieve the list of videos in the specified playlist.
+function requestVideoPlaylist(playlistId, pageToken) {
+  $('#video-container').html('');
+  var requestOptions = {
+    playlistId: playlistId,
+    part: 'snippet',
+    maxResults: 10
+  };
+  if (pageToken) {
+    requestOptions.pageToken = pageToken;
+  }
+  var request = gapi.client.youtube.playlistItems.list(requestOptions);
+  request.execute(function(response) {
+    // Only show pagination buttons if there is a pagination token for the
+    // next or previous page of results.
+    nextPageToken = response.result.nextPageToken;
+    var nextVis = nextPageToken ? 'visible' : 'hidden';
+    $('#next-button').css('visibility', nextVis);
+    prevPageToken = response.result.prevPageToken
+    var prevVis = prevPageToken ? 'visible' : 'hidden';
+    $('#prev-button').css('visibility', prevVis);
+
+    var playlistItems = response.result.items;
+    if (playlistItems) {
+      $.each(playlistItems, function(index, item) {
+        displayResult(item.snippet);
+      });
+    } else {
+      $('#video-container').html('Sorry you have no uploaded videos');
+    }
+  });
+}
+
+$( "img.thumbnail" ).attr( "src", function() {
+  return "/resources/" + this.title;
+});
+
+// Create a listing for a video.
+function displayResult(videoSnippet) {
+  var title = videoSnippet.title;
+  var videoId = videoSnippet.resourceId.videoId;
+  var thumb = videoSnippet.thumbnails.default.url; 
+  $('#video-container').append('<div><img title="' + title + '" class="thumbnail" src="'+thumb+'" ></div>');
+  
+}
+
+// Get youtube link video.
+$('body').on('click','.thumbnail',function () {
+  var img = $(this).attr('src');
+  var imgSlit = img.split("/");
+  var urlYoutube = 'http://www.youtube.com/watch?v='+imgSlit[4];
+  $(".youtube_url", window.opener.document).val(urlYoutube);
+  $("#id_manualurl", window.opener.document).val(urlYoutube);
+  window.close();
+});
+
+// Retrieve the next page of videos in the playlist.
+function nextPage() {
+  requestVideoPlaylist(playlistId, nextPageToken);
+}
+
+// Retrieve the previous page of videos in the playlist.
+function previousPage() {
+  requestVideoPlaylist(playlistId, prevPageToken);
+}
---
+++ version.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains the version information for the onlinepoodll submission plugin
+ * Development funded by: Global Awakening (@link http://www.globalawakening.com)
+ *
+ * @package    assignsubmission_onlinepoodll
+ * @copyright 2012 Justin Hunt {@link http://www.poodll.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+$plugin->version   = 2013100600;
+$plugin->requires  = 2012062500;
+$plugin->component = 'assignsubmission_youtube'; 
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '1.3 (Build 2013100600)';
+
